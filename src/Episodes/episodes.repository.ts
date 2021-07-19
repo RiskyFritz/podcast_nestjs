40bafed09a20abc425a2f1f0f1a81a5edb17b8ee
Ethan Olsen <131eolsen@gmail.com>
2021-07-18 17:27:55 -0700

    add basic filter for episode RSS fetch add prettier tabwidth change for formatting

--- src/Episodes/episodes.repository.ts
+++ src/Episodes/episodes.repository.ts
@@ -1,37 +1,55 @@
 /* eslint-disable prettier/prettier */
-import { EntityRepository, Repository } from "typeorm";
-import { CreateEpisodesDto } from './dto/create-episodes.dto';
-import { Episodes } from "./episodes.entity";
-import { GetEpisodesFilterDto } from "./dto/get-episodes-filter.dto";
+import { EntityRepository, Repository } from 'typeorm'
+import { CreateEpisodesDto } from './dto/create-episodes.dto'
+import { Episodes } from './episodes.entity'
+import { GetEpisodesFilterDto } from './dto/get-episodes-filter.dto'
 
 @EntityRepository(Episodes)
 export class EpisodesRepository extends Repository<Episodes> {
     async getEpisodes(episodesDto: GetEpisodesFilterDto): Promise<Episodes[]> {
-        const { search } = episodesDto;
+        const { search } = episodesDto
 
-        const query = this.createQueryBuilder('episodes');
+        const query = this.createQueryBuilder('episodes')
 
         if (search) {
             query.andWhere(
                 'LOWER(episodes.title) LIKE LOWER(:search) OR LOWER(episodes.description) LIKE LOWER(:search)',
-                { search: `%${search}%`},
-            );
+                { search: `%${search}%` },
+            )
         }
-        const episodes = await query.getMany();
-        return episodes;
+        const episodes = await query.getMany()
+        return episodes
     }
-    async createEpisode(createEpisodesDto: CreateEpisodesDto): Promise<Episodes> {
-        const { title, description, audioUrl, pubDate, duration } = createEpisodesDto;
+    async createEpisode(
+        createEpisodesDto: CreateEpisodesDto,
+    ): Promise<Episodes> {
+        const {
+            title,
+            description,
+            audioUrl,
+            pubDate,
+            duration,
+        } = createEpisodesDto
 
         const episode = this.create({
-        title,
-        description,
-        audioUrl,
-        pubDate,
-        duration,
-        });
+            title,
+            description,
+            audioUrl,
+            pubDate,
+            duration,
+        })
 
-        await this.save(episode);
-        return episode;
+        await this.save(episode)
+        return episode
+    }
+
+    async getMostRecentEpisode(): Promise<Episodes> {
+        // get the most recently created episode
+        // the date will likely need to be put into the db as a date type so we can be confident in the return of this function
+        const query = await this.createQueryBuilder('episodes').getOne()
+        if (query) {
+            return query
+        }
+        return null
     }
 }
--- src/Episodes/episodes.service.ts
+++ src/Episodes/episodes.service.ts
@@ -1,31 +1,37 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { CreateEpisodesDto } from './dto/create-episodes.dto';
-import { GetEpisodesFilterDto } from './dto/get-episodes-filter.dto';
-import { EpisodesRepository } from './episodes.repository';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Episodes } from './episodes.entity';
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { CreateEpisodesDto } from './dto/create-episodes.dto'
+import { GetEpisodesFilterDto } from './dto/get-episodes-filter.dto'
+import { EpisodesRepository } from './episodes.repository'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Episodes } from './episodes.entity'
 
 @Injectable()
 export class EpisodesService {
     constructor(
         @InjectRepository(EpisodesRepository)
         private EpisodesRepository: EpisodesRepository,
-      ) {}
-    
-      getEpisodes(filterDto: GetEpisodesFilterDto): Promise<Episodes[]> {
-        return this.EpisodesRepository.getEpisodes(filterDto);
-      }
-    
-      async getEpisodeById(id: string): Promise<Episodes> {
-        const found = await this.EpisodesRepository.findOne(id);
+    ) {}
+
+    getEpisodes(filterDto: GetEpisodesFilterDto): Promise<Episodes[]> {
+        return this.EpisodesRepository.getEpisodes(filterDto)
+    }
+
+    async getEpisodeById(id: string): Promise<Episodes> {
+        const found = await this.EpisodesRepository.findOne(id)
         if (!found) {
-          throw new NotFoundException(`Task with ID "${id}" not found`);
+            throw new NotFoundException(`Task with ID "${id}" not found`)
         }
-    
-        return found;
-      }
-    
-      createEpisode(CreateEpisodesDto: CreateEpisodesDto): Promise<Episodes> {
-        return this.EpisodesRepository.createEpisode(CreateEpisodesDto);
-      }
+
+        return found
+    }
+
+    createEpisode(CreateEpisodesDto: CreateEpisodesDto): Promise<Episodes> {
+        return this.EpisodesRepository.createEpisode(CreateEpisodesDto)
+    }
+
+    async getMostRecentEpisode(): Promise<Episodes> {
+        const mostRecent = await this.EpisodesRepository.getMostRecentEpisode()
+
+        return mostRecent
+    }
 }
